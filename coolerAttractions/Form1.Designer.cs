a8bc9df194c1c63b84238fad1e3868b64b92a897
Francesca <fagal067@learner.dvusd.org>
2022-03-10 09:03:14 -0700

    meme

--- coolerAttractions/Form1.Designer.cs
+++ coolerAttractions/Form1.Designer.cs
@@ -341,6 +341,7 @@
             // 
             this.bindingNavigatorPositionItem.AccessibleName = "Position";
             this.bindingNavigatorPositionItem.AutoSize = false;
+            this.bindingNavigatorPositionItem.Font = new System.Drawing.Font("Segoe UI", 9F);
             this.bindingNavigatorPositionItem.Name = "bindingNavigatorPositionItem";
             this.bindingNavigatorPositionItem.Size = new System.Drawing.Size(50, 23);
             this.bindingNavigatorPositionItem.Text = "0";
